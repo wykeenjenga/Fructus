f521a1db6ef9ce7570d8338b73d2a8faba9ffcd0
wykee2 <wycliffnjenga19@gmail.com>
2021-11-27 15:25:05 +0300

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Wykee Njenga on 11/27/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
